cdb7c24299f178c4907f2653741f69d12d511505
juanlop1989 <157081271+juanlop1989@users.noreply.github.com>
2025-02-16 22:19:19 -0600

    Avance 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 import {
   ActionSheetController,
   AlertController,
@@ -19,14 +20,33 @@ import {
   IonList,
   IonTitle,
   IonToolbar,
+  IonInput,
+  
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonApp, IonRouterOutlet, IonCard, IonAlert, IonButton, IonCardHeader, IonCardContent, IonImg, IonCardTitle, IonCardSubtitle],
+  imports: [IonApp, IonRouterOutlet, IonCard, IonAlert, IonButton, IonCardHeader, IonCardContent, IonImg, IonCardTitle, IonLabel, IonInput, CommonModule],
 })
+
+
 export class HomePage {
+  num:number = 1;
+
   constructor() {}
+
+  sumarCantidad() {
+    this.num += 1;
+  }
+
+  restarCantidad() {
+    this.num -= 1;
+    if (this.num < 1) this.num = 1;
+  }
+
+  alerta(){
+    alert('Producto agregado al carrito');
+  }
 }
